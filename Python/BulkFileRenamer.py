d2daebfc4888da667511c713ec300efeac8a817b
Vincent Villatte <vincent@villatte.net>
2024-05-06 12:47:00 +0200

    Modified BulkFileRenamer.py

--- Python/BulkFileRenamer.py
+++ Python/BulkFileRenamer.py
@@ -1,8 +1,8 @@
 import os
 import re
 
-# # Define the directory where the files are located
-# directory = "I:\\Dump\\Grey's Anatomy (2005) Season 17 S17 (1080p AMZN WEB-DL x265 HEVC 10bit EAC3 5.1 Garshasp)"
+# Define the directory where the files are located
+directory = "I:\\Dump"
 
 # # Iterate over all files in the directory
 # for filename in os.listdir(directory):
@@ -16,24 +16,38 @@ import re
 #         print(f"{filename} --> {new_filename}")
 #         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
 
-# Define the directory where the files are located
-directory = "D:\\home\\vvillatte\\Torrents\\Attack on Titan\\Attack on Titan - Shingeki no Kyojin\\6. Attack on Titan S4 (2020)"
 
-# Iterate over the files in the directory
+# # Iterate over the files in the directory
+# for filename in os.listdir(directory):
+#     # If the filename matches the pattern
+#     # if re.match(r'\[NewbSubs\] Shingeki no Kyojin \d{2} \(1080p Blu-ray x265 Dual Audio\)\[\w{8}\]', filename):
+#     if re.match(r'\[NewbSubs\] Shingeki no Kyojin S4 \d{2} \(1080p x265 AAC\)\[\w{8}\].mkv', filename):
+#         # Extract the episode number
+#         episode_number = re.search(r'\d{2}', filename).group()
+
+#         # Construct the new filename
+#         # new_filename = f"Shingeki no Kyojin S01E{episode_number}.mkv"
+#         new_filename = f"Shingeki no Kyojin S04E{episode_number}.mkv"
+
+#         # Rename the file
+#         print(f"{filename} --> {new_filename}\r")
+#         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
+
+# # Print a success message
+# print("Files have been successfully renamed.")
+
 for filename in os.listdir(directory):
-    # If the filename matches the pattern
-    # if re.match(r'\[NewbSubs\] Shingeki no Kyojin \d{2} \(1080p Blu-ray x265 Dual Audio\)\[\w{8}\]', filename):
-    if re.match(r'\[NewbSubs\] Shingeki no Kyojin S4 \d{2} \(1080p x265 AAC\)\[\w{8}\].mkv', filename):
+    if filename.endswith(".mkv"):
         # Extract the episode number
-        episode_number = re.search(r'\d{2}', filename).group()
-
+        episode_num = re.search(r'\d+', filename).group()
+        # Format the episode number to have leading zeros
+        episode_num = episode_num.zfill(3)
         # Construct the new filename
-        # new_filename = f"Shingeki no Kyojin S01E{episode_number}.mkv"
-        new_filename = f"Shingeki no Kyojin S04E{episode_number}.mkv"
-
+        new_filename = f"One Piece S01E{episode_num}.mkv"
+        # Get the full original file path
+        original_file = os.path.join(directory, filename)
+        # Get the full new file path
+        new_file = os.path.join(directory, new_filename)
         # Rename the file
-        print(f"{filename} --> {new_filename}\r")
-        os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
-
-# Print a success message
-print("Files have been successfully renamed.")
+        # os.rename(original_file, new_file)
+        print(f"{original_file} --> {new_file}")
