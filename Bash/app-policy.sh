e7150c41f6edc81e2b7ed5cb366aa4f529d1800d
Vincent Villatte <vincent@villatte.net>
2024-05-27 22:32:35 +0200

    Updated notify-user.sh

--- Bash/app-policy.sh
+++ Bash/app-policy.sh
@@ -1,109 +1,101 @@
 #!/bin/bash
-# Force the script to be run as Super User or user with sudo permissions
-if [[ $EUID -ne 0 ]]; then
-        echo "This script must be run as root or with sudo permissions"
-        exit 1
+
+# Check if jq is installed
+if ! command -v jq &> /dev/null; then
+  echo "jq could not be found. Please install it and try again."
+  exit 1
 fi
 
-# Ensure there are exactly 5 parameters
-if [[ $# -ne 5 ]]; then
-        echo "Usage: $0 <app_path> <start_time> <end_time> <limit>"
-        echo "Example: $0 /usr/bin/discord \"17:00\" \"19:00\" \"120\""
-        exit 1
+# Define the location of the settings file
+settings_file="/usr/local/sbin/parental-controls/pc-settings.json"
+
+# Read the settings from the pc-settings.json file
+settings=$(cat $settings_file)
+username=$(echo $settings | jq -r '.user.username')
+#username="vvillatte"
+app_path=$(echo $settings | jq -r '.user.application.path')
+app_name=$(basename $app_path)
+#app_name="spotify"
+
+# Define the location of the log file
+log_dir="/var/log/parental-controls/"
+log_file="${log_dir}${username}-${app_name}.log"
+
+# Check if the username in the settings file is the same as the user running this script
+if [ "$username" != "$(whoami)" ]; then
+  echo "The username in the settings file does not match the user running this script."
+  exit 1
 fi
 
-username=$1
-app_path=$2
+# Get the current day of the week
+day=$(date +%A | tr '[:upper:]' '[:lower:]')
 
-# Validate the time format for the start and end times
-if ! [[ $3 =~ ^([01][0-9]|2[0-3]):[0-5][0-9]$ ]] || ! [[ $4 =~ ^([01][0-9]|2[0-3]):[0-5][0-9]$ ]]; then
-        echo "Invalid time format. Please use the format \"HH:MM\"."
-        exit 1
-fi
+# Get the application end time for the current day
+app_end=$(echo $settings | jq -r ".user.application.$day.end")
+echo "(DEBUG): Application end time: $app_end"
+app_end_seconds=$(date -d"$app_end" +%s)
+echo "(DEBUG): Application end time in seconds: $app_end_seconds"
+current_time_seconds=$(date +%s)
+echo "(DEBUG): Current time in seconds: $current_time_seconds"
 
-# Validate that limit is a positive integer
-if ! [[ $5 =~ ^[0-9]+$ ]]; then
-        echo "Invalid limit format. Please enter a positive integer representing the time limit in minutes."
-        exit 1
-fi
+# Calculate the remaining time until the application end time
+remaining_app_time=$((app_end_seconds - current_time_seconds))
+echo "(DEBUG): Application remaining time: $remaining_app_time"
 
-# Check if the username is valid
-if ! id "$username" >/dev/null 2>&1; then
-        echo "User '$username' does not exist"
-        exit 1
-fi
-
-# Check if the Application exists
-if [[ ! -e "$app_path" ]]; then
-        echo "The Application $app_path does not exist."
-        exit 1
-fi
-
-# Set the target_path application path, following any symlink if required
-target_path="$app_path"
-if [[ -L "$app_path" ]]; then
-        target_path=$(readlink -f "$app_path")
-fi
-
-# Set the application name
-app_name=$(basename "$app_path")
-
-# Set the log file name
-log_file=$"/var/log/$username-$app_name.log"
-
-echo "Applying execution policy to $app_name for $username"
-
-# Get the current hour and minute
-current_time=$(date +%s)
-
-# Define the start and end times in Unix timestamp format
-start_time=$(date -d"$3" +%s)
-end_time=$(date -d"$4" +%s)
-# Define the time limit in minutes
-limit=$5
-
-# Check if the Application is executable and store the result as true or false
-is_executable=$( [[ -x "$app_path" ]] && echo true || echo false )
-
-# Check if the Application is running and store the result as true or false
 is_running=$(/usr/bin/pgrep -fu "$username" "$app_name" > /dev/null && echo true || echo false)
+#is_running=$(pgrep -u $username -x $app_name > /dev/null && echo true || echo false)
 
-# Get the application runtime from log file
-if [[ -e "$log_file" ]]; then
-        total_runtime=$(grep "Total Daily Runtime:" $log_file | cut -d ' ' -f4)
-        echo "Application $app_name has been used for $total_runtime minutes"
-else
-        echo "Log file $log_file not found"
+# Check if the application is running for this user
+if [[ $is_running == "true" ]]; then
+        # Check if the application is getting close to being closed due to the end time
+        if ((remaining_app_time <= 60)); then
+                notify-send "Warning: The application $app_name will close in 1 minute due to the end time."
+        elif ((remaining_app_time <= 300)); then
+                notify-send "Warning: The application $app_name will close in 5 minutes due to the end time."
+        elif ((remaining_app_time <= 600)); then
+                notify-send "Warning: The application $app_name will close in 10 minutes due to the end time."
+        fi
 fi
 
-if (( total_runtime >= limit )); then
-        if [[ $is_executable == "true" ]]; then
-                chmod -x $app_path
-                echo "Application execution disabled"
+# Get the application limit for the current day
+app_limit=$(echo $settings | jq -r ".user.application.$day.limit")
+echo "(DEBUG): Application limit: $app_limit"
+
+# Read the log file
+log=$(cat $log_file)
+
+# Get the total daily runtime
+total_runtime=$(echo $log | grep -oP 'Total Daily Runtime: \K\d+')
+echo "(DEBUG): Total runtime: $total_runtime"
+
+# Calculate the remaining time
+remaining_time=$((app_limit - total_runtime))
+echo "(DEBUG): Remaining time: $remaining_time"
+
+if [[ $is_running == "true" ]]; then
+        # Check if the application is getting close to being killed and disabled
+        if ((remaining_time <= 1)); then
+          notify-send "Warning: The application $app_name will close in 1 minute."
+        elif ((remaining_time <= 5)); then
+          notify-send "Warning: The application $app_name will close in 5 minutes."
+        elif ((remaining_time <= 10)); then
+          notify-send "Warning: The application $app_name will close in 10 minutes."
         fi
-        if [[ $is_running == "true" ]]; then
-                echo "(DEBUG) Killing the applicaion"
-                /usr/bin/pkill -fu "$username" "$app_name"
-                echo "The application was running more than the allowed duration and has been killed"
-        fi
-elif (( current_time >= start_time && current_time < end_time )); then
-        if [[ $is_executable == "true" ]]; then
-                echo "Application already executable"
-        else
-                chmod +x $app_path
-                echo "Application execution enabled"
-        fi
-else
-        if [[ $is_executable == "true" ]]; then
-                chmod -x $app_path
-                echo "Application execution disabled"
-        else
-                echo "Application already non-executable"
-        fi
-        if [[ $is_running == "true" ]]; then
-                /usr/bin/pkill -fu "$username" "$app_name"
-                echo "Application $app_name was running outside the allowed time range and has been killed"
-        else
-                echo "Application is not running"
-        fi
+fi
+
+# Do the same for the login end time
+login_end=$(echo $settings | jq -r ".user.login.$day.end")
+login_end_seconds=$(date -d"$login_end" +%s)
+current_time_seconds=$(date +%s)
+
+# Calculate the remaining login time
+remaining_login_time=$((login_end_seconds - current_time_seconds))
+
+# Check if the login time is getting close to ending
+if ((remaining_login_time <= 60)); then
+  notify-send "Warning: The login time will end in 1 minute."
+elif ((remaining_login_time <= 300)); then
+  notify-send "Warning: The login time will end in 5 minutes."
+elif ((remaining_login_time <= 600)); then
+  notify-send "Warning: The login time will end in 10 minutes."
 fi
