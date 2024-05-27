fa7ec96cb09845caed216b263f2dc5597861fa74
Vincent Villatte <vincent@villatte.net>
2024-05-27 19:07:58 +0200

    Fixed issue with app-policy not reading the limit parameter

--- Bash/app-policy.sh
+++ Bash/app-policy.sh
@@ -5,7 +5,7 @@ if [[ $EUID -ne 0 ]]; then
         exit 1
 fi
 
-# Ensure there are exactly three parameters
+# Ensure there are exactly 5 parameters
 if [[ $# -ne 5 ]]; then
         echo "Usage: $0 <app_path> <start_time> <end_time> <limit>"
         echo "Example: $0 /usr/bin/discord \"17:00\" \"19:00\" \"120\""
@@ -21,6 +21,12 @@ if ! [[ $3 =~ ^([01][0-9]|2[0-3]):[0-5][0-9]$ ]] || ! [[ $4 =~ ^([01][0-9]|2[0-3
         exit 1
 fi
 
+# Validate that limit is a positive integer
+if ! [[ $5 =~ ^[0-9]+$ ]]; then
+        echo "Invalid limit format. Please enter a positive integer representing the time limit in minutes."
+        exit 1
+fi
+
 # Check if the username is valid
 if ! id "$username" >/dev/null 2>&1; then
         echo "User '$username' does not exist"
@@ -33,14 +39,17 @@ if [[ ! -e "$app_path" ]]; then
         exit 1
 fi
 
-target="$app_path"
-app_name=$(basename "$app_path")
-log_file=$"/var/log/$username-$app_name.log"
-
-# Check for Symlink
+# Set the target_path application path, following any symlink if required
+target_path="$app_path"
 if [[ -L "$app_path" ]]; then
-        target=$(readlink -f "$app_path")
+        target_path=$(readlink -f "$app_path")
 fi
+
+# Set the application name
+app_name=$(basename "$app_path")
+
+# Set the log file name
+log_file=$"/var/log/$username-$app_name.log"
 
 echo "Applying execution policy to $app_name for $username"
 
@@ -50,28 +59,34 @@ current_time=$(date +%s)
 # Define the start and end times in Unix timestamp format
 start_time=$(date -d"$3" +%s)
 end_time=$(date -d"$4" +%s)
+# Define the time limit in minutes
+limit=$5
 
 # Check if the Application is executable and store the result as true or false
 is_executable=$( [[ -x "$app_path" ]] && echo true || echo false )
 
 # Check if the Application is running and store the result as true or false
-is_running=$(pgrep -fu "$username" "$app_name" > /dev/null && echo true || echo false)
+is_running=$(/usr/bin/pgrep -fu "$username" "$app_name" > /dev/null && echo true || echo false)
 
-# Read the Total Daily Runtime from application log
+# Get the application runtime from log file
 if [[ -e "$log_file" ]]; then
         total_runtime=$(grep "Total Daily Runtime:" $log_file | cut -d ' ' -f4)
         echo "Application $app_name has been used for $total_runtime minutes"
-        # Kill the application if it has run more than the allowed time
-        if (( total_runtime >= limit )) && [[ $is_runnning == "true" ]]; then
-                /usr/bin/pkill -fu "$username" "$app_name"
-                echo "The application was running more than the allowed duration and has been killed"
-        fi
 else
         echo "Log file $log_file not found"
 fi
 
-# Set the application executable flag as per policy
-if (( current_time >= start_time && current_time < end_time )); then
+if (( total_runtime >= limit )); then
+        if [[ $is_executable == "true" ]]; then
+                chmod -x $app_path
+                echo "Application execution disabled"
+        fi
+        if [[ $is_running == "true" ]]; then
+                echo "(DEBUG) Killing the applicaion"
+                /usr/bin/pkill -fu "$username" "$app_name"
+                echo "The application was running more than the allowed duration and has been killed"
+        fi
+elif (( current_time >= start_time && current_time < end_time )); then
         if [[ $is_executable == "true" ]]; then
                 echo "Application already executable"
         else
