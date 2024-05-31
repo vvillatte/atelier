ceb973899b911b1cf3c43896449c7fa53fc379cf
Vincent Villatte <vincent@villatte.net>
2024-05-31 22:39:03 +0200

    Adde web-policy.sh

--- Bash/pc.sh
+++ Bash/pc.sh
@@ -29,6 +29,9 @@ app_path=$(jq -r '.user.application.path' "$config_file")
 app_usage_start=$(jq -r ".user.application.$current_day.start" "$config_file")
 app_usage_end=$(jq -r ".user.application.$current_day.end" "$config_file")
 app_usage_limit=$(jq -r ".user.application.$current_day.limit" "$config_file")
+url=$(jq -r 'user.website.url' "$config_file")
+url_usage_start=$(jq -r ".user.website.$current_day.start" "$config_file")
+url_usage_end=$(jq -r ".user.website.$current_day.end" "$config_file")
 
 # Print the scheduling rules for the current day
 echo "$current_day Login Start Time: $login_start"
@@ -37,7 +40,12 @@ echo "Application Path: $app_path"
 echo "$current_day Application Usage Start Time: $app_usage_start"
 echo "$current_day Application Usage End Time: $app_usage_end"
 echo "$current_day Application Usage Limit: $app_usage_limit"
+echo "Website url: $url"
+echo "$current_day Website Usage Start Time: $app_usage_start"
+echo "$current_day Website Usage End Time: $app_usage_end"
 
 # Apply program and login rules
 "$dir_path/app-policy.sh" "$username" "$app_path" "$app_usage_start" "$app_usage_end" "$app_usage_limit"
+"$dir_path/web-policy.sh" "$username" "$url" "$url_usage_start" "$url_usage_end"
+/usr/sbin/squid -k reconfigure
 "$dir_path/login-policy.sh" "$username" "$login_start" "$login_end"
---
+++ Bash/web-policy.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+# Force the script to be run as Super User or user with sudo permissions
+if [[ $EUID -ne 0 ]]; then
+    echo "This script must be run as root or with sudo permissions"
+    exit 1
+fi
+
+# Ensure there are exactly 3 parameters
+if [[ $# -ne 3 ]]; then
+    echo "Usage: $0 <url> <start_time> <end_time>"
+    echo "Example: $0 .domain.tld \"17:00\" \"19:00\""
+    exit 1
+fi
+
+# Validate the time format for the start and end times
+if ! [[ $2 =~ ^([01][0-9]|2[0-3]):[0-5][0-9]$ ]] || ! [[ $3 =~ ^([01][0-9]|2[0-3]):[0-5][0-9]$ ]]; then
+    echo "Invalid time format. Please use the format \"HH:MM\"."
+    exit 1
+fi
+
+url=$1
+acl_path="/etc/squid/domains_whitelist.acl"
+regex='^(#?\.[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6})$'
+
+# Check that the url is well formed
+if [[ ! $url =~ $regex ]]; then
+    echo "The URL is not well-formed"
+    exit 1
+fi
+
+# Check if the url exists in domain_whitelist.acl
+if ! grep -E "$url" "$acl_path"; then
+    echo "Adding domain $url to $acl_path"
+    echo "$url" >> "$acl_path"
+fi
+
+# Get the current hour and minute
+current_time=$(date +%s)
+
+# Define the start and end times in Unix timestamp format
+start_time=$(date -d"$2" +%s)
+end_time=$(date -d"$3" +%s)
+
+# Check if time is in range and if the url is commented out
+if (( current_time >= start_time && current_time < end_time )); then
+    if grep -Eq "^#.*$url" "$acl_path"; then
+        sed -i "/^#.*$url/s/^#//" "$acl_path"
+        echo "Domain $url is accessible"
+    else
+        echo "Domain $url already accessible"
+    fi
+else
+    if grep -Eq "^#.*$url" "$acl_path"; then
+        echo "Domain $url already blocked"
+    else
+        sed -i "/^$url/s/^/#/" "$acl_path"
+        echo "Domain $url blocked"
+    fi
+fi
+
+# Update squid configuration without restarting the server
+#/usr/sbin/squid -k reconfigure
