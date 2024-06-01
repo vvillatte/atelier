246a7cdf1df6359bb67eec3615c5358717f058bc
Vincent Villatte <vincent@villatte.net>
2024-06-01 12:21:07 +0200

    Updated json and pc.sh to read it

--- Bash/pc.sh
+++ Bash/pc.sh
@@ -21,26 +21,34 @@ fi
 # Get the current day of the week
 current_day=$(date +%A | tr '[:upper:]' '[:lower:]')
 
+# Read the entire JSON file into a variable
+config=$(cat "$config_file")
+
 # Read the login and application usage schedule for the current day
-username=$(jq -r '.user.username' "$config_file")
-login_start=$(jq -r ".user.login.$current_day.start" "$config_file")
-login_end=$(jq -r ".user.login.$current_day.end" "$config_file")
-app_path=$(jq -r '.user.application.path' "$config_file")
-app_usage_start=$(jq -r ".user.application.$current_day.start" "$config_file")
-app_usage_end=$(jq -r ".user.application.$current_day.end" "$config_file")
-app_usage_limit=$(jq -r ".user.application.$current_day.limit" "$config_file")
-url=$(jq -r '.user.website.url' "$config_file")
-url_usage_start=$(jq -r ".user.website.$current_day.start" "$config_file")
-url_usage_end=$(jq -r ".user.website.$current_day.end" "$config_file")
+username=$(jq -r '.user.login.name' <<< "$config")
+login_start=$(jq -r ".user.login.schedule.$current_day.start" <<< "$config")
+login_end=$(jq -r ".user.login.schedule.$current_day.end" <<< "$config")
+app_name=$(jq -r '.user.application.name' <<< "$config")
+app_path=$(jq -r '.user.application.path' <<< "$config")
+app_usage_start=$(jq -r ".user.application.schedule.$current_day.start" <<< "$config")
+app_usage_end=$(jq -r ".user.application.schedule.$current_day.end" <<< "$config")
+app_usage_limit=$(jq -r ".user.application.schedule.$current_day.limit" <<< "$config")
+site_name=$(jq -r '.user.website.name' <<< "$config")
+site_url=$(jq -r '.user.website.url' <<< "$config")
+site_usage_start=$(jq -r ".user.website.schedule.$current_day.start" <<< "$config")
+site_usage_end=$(jq -r ".user.website.schedule.$current_day.end" <<< "$config")
 
 # Print the scheduling rules for the current day
+echo "User name: $username"
 echo "$current_day Login Start Time: $login_start"
 echo "$current_day Login End Time: $login_end"
+echo "Application anme: $app_name"
 echo "Application Path: $app_path"
 echo "$current_day Application Usage Start Time: $app_usage_start"
 echo "$current_day Application Usage End Time: $app_usage_end"
 echo "$current_day Application Usage Limit: $app_usage_limit"
-echo "Website url: $url"
+echo "Website: $site_name"
+echo "Website url: $site_url"
 echo "$current_day Website Usage Start Time: $app_usage_start"
 echo "$current_day Website Usage End Time: $app_usage_end"
 echo "=== *** ==="
@@ -48,7 +56,7 @@ echo "=== *** ==="
 # Apply program and login rules
 "$dir_path/app-policy.sh" "$username" "$app_path" "$app_usage_start" "$app_usage_end" "$app_usage_limit"
 echo "=== *** ==="
-"$dir_path/web-policy.sh" "$url" "$url_usage_start" "$url_usage_end"
+"$dir_path/web-policy.sh" "$site_url" "$site_usage_start" "$site_usage_end"
 sudo /usr/sbin/squid -k reconfigure
 echo "=== *** ==="
 "$dir_path/login-policy.sh" "$username" "$login_start" "$login_end"
